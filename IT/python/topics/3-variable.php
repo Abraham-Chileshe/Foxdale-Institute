6b958c2a98dbd2731ea317edf4b5106248c372f1
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-09-22 01:54:07 +1000

    Update 3-variable.php

--- IT/python/topics/3-variable.php
+++ IT/python/topics/3-variable.php
@@ -5,12 +5,62 @@
                 
         <div style="overflow-y: scroll; height:700px;" class="col-lg-7 col-md-6 wow fadeInUp" data-wow-delay="0.1s">  
             <div class="container-md"><br/>
-                <h3 class="text-primary">What is Python</h3>
+                <h3 class="text-primary">What are variables?</h3>
+                <p>Variables are Containers for Storing Data</p><br />
+                <center><img src="topic_img/variable.png" style="width:400px; max-width:95%"/></center>
+                <br /><br />
+                <h3 class="text-primary">Creating a variable</h3>
+                <p>A variable is created the moment you first assign a value to it. A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:</p>
                 
+<pre>           
+<code class="language-python">
+x = 5
+y = &quot;John&quot;
+print(x)
+print(y)
+</code>
+</pre>            
+                <p><i>Copy this code and run it in the interpreter.</i></p>
+                <p>Variables do not need to be declared with any particular type, and can even change type after they have been set.</p>
+<pre>
+<code class="language-python">
+x = 4   
+x = &quot;Marco Jay&quot; 
+print(x)
+</code>
+</pre>            
+
+                <h3 class="text-primary">Rules for making variables</h3>
+                <ul>
+                    <li>A variable name must start with a letter or the underscore characte</li> 
+                    <li>A variable name cannot start with a number</li>    
+                    <li>A variable cannot contain symbols like (+ - & ^ % $ #) etc</li> 
+                    <li>A variable name cannot contain space. you use underscore to connect two words</li>      
+                </ul>
+                <p><i>Accepted ways of making variables</i></p>
+<pre>
+<code class="language-python">
+myvar = &quot;John&quot;
+my_var = &quot;John&quot;
+_my_var = &quot;John&quot;
+myVar = &quot;John&quot;
+MYVAR = &quot;John&quot;
+myvar2 = &quot;John&quot;
+</code>
+</pre>
+                <p><i>Illegal ways of making variables</i></p>
+<pre>
+<code class="language-python">  
+2myvar = &quot;John&quot;
+my-var = &quot;John&quot;
+my var = &quot;John&quot   
+</code>
+</pre>
             </div>
         </div>
 
         <div class="col-lg-5 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        <iframe src="https://trinket.io/embed/python/7e3f290f96?showInstructions=true"  width="100%" height="700" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
         </div>
                    
     </div>
