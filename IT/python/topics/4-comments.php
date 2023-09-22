774793223895d70900d46fdfc74b07c081bbfb6a
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2023-09-22 15:04:08 +1000

    Update 4-comments.php

--- IT/python/topics/4-comments.php
+++ IT/python/topics/4-comments.php
@@ -5,12 +5,42 @@
                 
         <div style="overflow-y: scroll; height:700px;" class="col-lg-7 col-md-6 wow fadeInUp" data-wow-delay="0.1s">  
             <div class="container-md"><br/>
-                <h3 class="text-primary">What is Python</h3>
-                
+                <h3 class="text-primary">What is comments</h3>
+                <p>Comments can be used to explain Python code. Comments can be used to make the code more readable. Comments can also be used to prevent execution when testing code.</p>
+                <br />
+
+                <h3 class="text-primary">Creating a comment</h3>
+                <p>Comments starts with a #, anything after this symbol will be considered part of the comment</p>
+            
+<pre>
+<code class="language-python">  
+#This is a comment
+print(&quot;Hello, World!&quot;)
+</code>
+</pre>
+
+                <p>Comments can be placed at the end of a line, and Python will ignore the rest of the line:</p>
+<pre>
+<code class="language-python">  
+print("Hello, World!") #This is a comment
+</code>
+</pre>
+
+                <p>A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:</p>
+<pre>
+<code class="language-python">  
+#print(&quot;Hello, World!&quot;)
+print(&quot;Cheers, Mate!&quot;)
+</code>
+</pre><br/>
+
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/J8K-v67o5vE?si=4yL-w2oZSAC0mYzB&amp;start=37" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
 
+
         <div class="col-lg-5 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        <iframe src="https://trinket.io/embed/python/38560c0921" width="100%" height="700" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
           </div>
                    
     </div>
